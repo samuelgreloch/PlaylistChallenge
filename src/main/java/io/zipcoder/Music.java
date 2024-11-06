40ac2bd95a77b41bd9a0efc7a3b499f95ef0baca
Samuel Greloch <sgreloch@Zipcoders-MacBook-Pro-4.local>
2024-11-06 15:36:44 -0500

    no

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -4,11 +4,42 @@ public class Music {
 
     private String[] playList;
 
-    public Music(String[] playList){
+    public Music(String[] playList) {
         this.playList = playList;
     }
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+        String[] playlist = {"wheniseeyouagain", "borntorun", "nothingelsematters", "cecelia "};
+
+         int jumpsForward = -1;
+         int jumpsBackward = -1;
+        for (int i = startIndex; i < playList.length; i++) {
+            if (playList[i] != selection) {
+                jumpsForward += 1;
+                jumpsBackward += 1;
+            }
+        }
+        if (jumpsForward < playList.length + jumpsForward && jumpsBackward < playList.length +jumpsBackward) {
+            return jumpsForward;
+
+        } else {
+            return playlist.length + jumpsBackward;
+
+        }
+
+
+
+
+
+        }
+
+
+
+
     }
-}
+
+
+
+
+
